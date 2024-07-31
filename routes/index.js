c07068c3c172e009b2bcd654a79e1cdc1c8f63d2
Marc Wright <marc.wright@secturasoft.com>
2024-07-30 19:47:11 -0500

    feature branch test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Diesel Express' });
+  res.render('index', { title: 'Diesel Express freature branch' });
 });
 
 module.exports = router;
